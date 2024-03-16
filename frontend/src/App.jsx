7bcc38ce3353ac695692a781c6efac16007a8e58
jainam-b <jainambagrecha16@gmail.com>
2024-03-16 17:28:53 +0530

    payment frontend running

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,29 +7,32 @@ import Payment from "./components/Payments";
 import Landing from "./components/Landing/Landing";
 import FirstScreen from "./components/Landing/FirstScreen";
 import Products from "./components/Productcard";
-import MenuItem from "./components/MenuItems";
+// import MenuItem from "./components/MenuItems";
 import NavBar from './components/NavBar/NavBar';
 import Login from './components/Login/Login';
 import Signup from './components/Signup/Signup';
-
+import { UserProvider } from './components/Context/UserContext ';
+import Items from "./components/Items/Items";
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <CartProvider> {/* Wrap the entire application with CartProvider */}
+     <UserProvider>
       <BrowserRouter>
         <Routes>
           <Route path="/payment" element={<Payment />} />
-     
+          <Route path="/item" element={<Items />} />
           <Route path="/" exact element={<Landing />} />
           <Route path="/try" exact element={<FirstScreen />} />
           <Route path="/cart" element={<CartPage />} />
           <Route path="/product" element={<Products />} />
-          <Route path="/menu-items" element={<MenuItem />} />
+          {/* <Route path="/menu-items" element={<MenuItem />} /> */}
           <Route path="/signup" element={<Signup />}></Route>
            <Route path="/login" element={<Login />}></Route>
         </Routes>
       </BrowserRouter>
+      </UserProvider>
     </CartProvider>
   );
 }
--- frontend/src/components/Cart.jsx
+++ frontend/src/components/Cart.jsx
@@ -1,49 +1,131 @@
 // CartPage.js
-import React from 'react';
-import { useCart } from './Context/CartContext';
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-import Button from '@mui/material/Button';
+import React,{useState, useEffect} from "react";
+import { useCart } from "./Context/CartContext";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import Button from "@mui/material/Button";
 import { useNavigate } from "react-router-dom";
-import Itemcard from './Items/Itemcard';
+import Itemcard from "./Items/Itemcard";
+import axios from "axios";
+import { useUser } from "./Context/UserContext ";
 
 const CartPage = () => {
   const { cartItems, removeFromCart, clearCart } = useCart();
-  
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  const calculateTotalPrice = () => {
+    let totalPrice = 0; // Initialize totalPrice variable
+
+    cartItems.forEach((item) => {
+      totalPrice += parseFloat(item.price); // Convert price to number if it's a string
+    });
+
+    setTotalPrice(totalPrice.toFixed(2)); // Round the total price to 2 decimal places
+  };
+
+  useEffect(() => {
+    calculateTotalPrice();
+  }, [cartItems]);
+ 
+  function loadScript(src) {
+    return new Promise((resolve) => {
+      const script = document.createElement("script");
+      script.src = src;
+      script.onload = () => {
+        resolve(true);
+      };
+      script.onerror = () => {
+        resolve(false);
+      };
+      document.body.appendChild(script);
+    });
+  }
+
+  async function displayRazorpay() {
+    const res = await loadScript(
+        "https://checkout.razorpay.com/v1/checkout.js"
+    );
+
+    if (!res) {
+        alert("Razorpay SDK failed to load. Are you online?");
+        return;
+    }
+
+    const result = await axios.post("http://localhost:3000/pay/orders", {
+        amount: 1000, // Update the amount with the new amount
+    });
+
+    if (!result) {
+        alert("Server error. Are you online?");
+        return;
+    }
+
+    const { amount, id: order_id, currency } = result.data;
+
+    const options = {
+        key: "rzp_test_YMSg1NivuORLa3", // Enter the Key ID generated from the Dashboard
+        amount: amount.toString(),
+        currency: currency,
+        name: "Soumya Corp.",
+        description: "Test Transaction",
+         
+        order_id: order_id,
+        handler: async function (response) {
+            const data = {
+                orderCreationId: order_id,
+                razorpayPaymentId: response.razorpay_payment_id,
+                razorpayOrderId: response.razorpay_order_id,
+                razorpaySignature: response.razorpay_signature,
+            };
+
+            const result = await axios.post("http://localhost:5000/payment/success", data);
+
+            alert(result.data.msg);
+        },
+        prefill: {
+            name: "Soumya Dey",
+            email: "SoumyaDey@example.com",
+            contact: "9999999999",
+        },
+        notes: {
+            address: "Soumya Dey Corporate Office",
+        },
+        theme: {
+            color: "#61dafb",
+        },
+    };
+
+    const paymentObject = new window.Razorpay(options);
+    paymentObject.open();
+}
   return (
     <div>
-     
-     
       <button onClick={clearCart}>Clear Cart</button>
       <div>
-      <div className='Items'>
-        
-        <h3>Cart Items</h3>
-        <div className='productMenu'>
-          {cartItems.length > 0 ? (
-            cartItems.map((item, index) => (
-              <Itemcard
-                key={index}
-                productId={item._id}
-               
-                image="sandwich.png" // Replace with the actual image URL
-                productName={item.productName}
-                // rating={item.productName} // Assuming rating is a property of the product object
-                price={`₹${item.price}`} // Assuming price is a property of the product object
-              />
-            ))
-          ) : (
-            <p>No products found</p>
-          )}
-        </div>
-        {/* <div className='moreProduct text-white'>
-        <Button variant=" " onClick={()=>{}} className='Cart' > More Products
-          </Button>
-          <div>
-            <ArrowForwardIosIcon className='ArrowForwardIosIcon' />
+        <div className="Items">
+          <h3>Cart Items</h3>
+          <div className="productMenu">
+            {cartItems.length > 0 ? (
+              cartItems.map((item, index) => (
+                <Itemcard
+                  key={index}
+                  productId={item._id}
+                  image="sandwich.png" // Replace with the actual image URL
+                  productName={item.productName}
+                  // rating={item.productName} // Assuming rating is a property of the product object
+                  price={`₹${item.price}`} // Assuming price is a property of the product object
+                />
+              ))
+            ) : (
+              <p>No products found</p>
+            )}
           </div>
-        </div> */}
+
+          <div>Total Price: {totalPrice}</div>
+        </div>
       </div>
-    </div>
+      <Button variant="contained" onClick={displayRazorpay} className="Cart">
+        Pay
+      </Button>
     </div>
   );
 };
