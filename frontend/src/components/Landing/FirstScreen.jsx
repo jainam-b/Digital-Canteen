7ac693fa274d8b5fae1380015fe25962df8de5f8
Kartikeya167 <163307365+kartikeya-Sh@users.noreply.github.com>
2024-03-24 13:34:57 +0530

    FirstScreen

--- frontend/src/components/Landing/FirstScreen.jsx
+++ frontend/src/components/Landing/FirstScreen.jsx
@@ -1,4 +1,3 @@
-// FirstScreen.js
 import React from 'react';
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
@@ -9,13 +8,8 @@ import './FirstScreen.css';
 function FirstScreen() {
   return (
     <div className="container">
-      <Stack
-        direction={{ xs: 'column', sm: 'row' }}
-        justifyContent="center"
-        alignItems="center"
-        spacing={2}
-      >
-        <Box sx={{ width: { xs: '100%', sm: '50%' } }} className="sectionOne">
+      <div className="background-image">
+        <Box className="sectionOne">
           <h1 className='HeadingVal'>
             <div className='text1'>Order <br /> Food <span className='HeadDiffColor'>with Ease</span> </div>
           </h1>
@@ -26,17 +20,17 @@ function FirstScreen() {
             <Button variant="contained" className='OrderButton' onClick={()=>{window.location.href="/item"}}>Order Now</Button>
           </div>
         </Box>
-        <Box sx={{ width: { xs: '100%', sm: '50%' } }} className="sectionTwo"onClick={()=>{window.location.href="/item"}}>
-          <div className='foodBox'>
-            <FSCard className='img1' cardImage={'SubscribeBurger.png'} ImageHead={'Burger'} Desp={'Mushroom Sauce '} Price={'$5.15'} />
-            <FSCard className='img2' cardImage={'junkfood.png'} ImageHead={'Food Combo'} Desp={'Mushroom Sauce '} Price={'$9.15'} />
-          </div>
-          <div className='foodBox'>
-            <FSCard cardImage={'SubscribeBurger.png'} ImageHead={'Burger'} Desp={'Mushroom Sauce '} Price={'$5.15'} />
-            <FSCard cardImage={'SubscribeBurger.png'} ImageHead={'Burger'} Desp={'Mushroom Sauce '} Price={'$5.15'} />
-          </div>
-        </Box>
-      </Stack>
+      </div>
+      <Box className="sectionTwo" onClick={()=>{window.location.href="/item"}}>
+        <div className='foodBox'>
+          <FSCard className='img1' cardImage={'SubscribeBurger.png'} ImageHead={'Burger'} Desp={'Mushroom Sauce '} Price={'$5.15'} />
+          <FSCard className='img2' cardImage={'junkfood.png'} ImageHead={'Food Combo'} Desp={'Mushroom Sauce '} Price={'$9.15'} />
+        </div>
+        <div className='foodBox'>
+          <FSCard cardImage={'SubscribeBurger.png'} ImageHead={'Burger'} Desp={'Mushroom Sauce '} Price={'$5.15'} />
+          <FSCard cardImage={'SubscribeBurger.png'} ImageHead={'Burger'} Desp={'Mushroom Sauce '} Price={'$5.15'} />
+        </div>
+      </Box>
     </div>
   );
 }
